fda6b87c89dbf3cfd3e5f7e27e37f16033d3cb97
RacoonYo <racoonyodev@gmail.com>
2022-04-07 00:18:47 +0300

    done task 3

--- Pashnik_Egor_dz_1/task3.py
+++ Pashnik_Egor_dz_1/task3.py
@@ -14,8 +14,19 @@
 
 def transform_string(number: int) -> str:
     """Возвращает строку вида 'N процентов' с учётом склонения по указанному number"""
-    # место для Вашего кода
-    return 'верните отформатированную строку'
+    procents = ''
+
+    if 0 < number <= 10 or 20 <= number:
+        if number % 10 == 1:
+            procents = 'процент'
+        elif number % 10 == 2 or number % 10 == 3 or number % 10 == 4:
+            procents = 'процента'
+        else:
+            procents = 'процентов'
+    elif 10 < number < 20:
+        procents = 'процентов'
+
+    return f'{number} {procents}'
 
 if __name__ == '__main__':
     for n in range(1, 101):  # по заданию учитываем только значения от 1 до 100
